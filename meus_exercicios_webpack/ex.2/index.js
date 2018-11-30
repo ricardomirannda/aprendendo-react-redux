66103e1299d775718a474bce20bf5f51d9f8f2c4
Ricardo Miranda <ricardomirannda@gmail.com>
2018-11-30 11:22:20 -0300

    Adicionando modulo do React ao Webpack

--- meus_exercicios_webpack/ex.2/index.js
+++
@@ -1,3 +0,0 @@
-console.log('Webpack');
-
-require('./duvidaCruel');
--- meus_exercicios_webpack/ex.3/index.js
+++
@@ -1,5 +0,0 @@
-import Pessoa from './pessoa'
-
-const pessoa = new Pessoa ('Ricardo Miranda - Frontend frf');
-console.log(pessoa.toString());
-
