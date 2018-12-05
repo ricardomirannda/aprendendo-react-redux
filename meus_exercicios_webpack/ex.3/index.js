4ddfc62d02c1db1966c55be5cd7a61200e56e48e
Ricardo Miranda <ricardomirannda@gmail.com>
2018-12-04 22:10:17 -0300

    todos os exercicios sobre webpack

---
+++ meus_exercicios_webpack/ex.3/index.js
@@ -0,0 +1,5 @@
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa ('Ricardo Miranda - Frontend frf');
+console.log(pessoa.toString());
+
--- meus_exercicios_webpack/webpack.config.js
+++ meus_exercicios_webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
@@ -9,16 +10,22 @@ module.exports = {
     devServer: {
         port: 3000,
         contentBase: './public'
-    },
+    }, plugins : [
+        new ExtractTextPlugin('app.css')
+    ],
     module: {
-        loaders: [{
+        loaders: [
+            {
             test: /.js?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
-            }
-        }]
+                plugins: ['transform-object-rest-spread']}
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            }]
     }
 }
