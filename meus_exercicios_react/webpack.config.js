84083bd36358b00d43f8e7a4b50309abddf4dfe1
Ricardo Miranda <ricardomirannda@gmail.com>
2018-12-10 19:21:27 -0300

    Primeiro componente usando funcoes

---
+++ meus_exercicios_react/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require ('webpack');
+
+module.exports = {
+    entry:  './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_module/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+
+        }]
+    }
+}
